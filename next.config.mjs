86ab827f0b9d84fbb8dc2a452b6026df6a8dd4e8
Salar Safayi <108089493+zalar2202@users.noreply.github.com>
2026-02-14 16:42:31 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,17 @@ const nextConfig = {
         minimumCacheTTL: 3600,
         deviceSizes: [640, 750, 828, 1080, 1200, 1920],
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+        localPatterns: [
+            {
+                pathname: '/api/files/**',
+            },
+            {
+                pathname: '/assets/**',
+            },
+            {
+                pathname: '/uploads/**',
+            },
+        ],
         remotePatterns: [
             {
                 protocol: 'https',
